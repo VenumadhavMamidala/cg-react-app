0b5c6f1165f368f294dbfaa4f79c15e28b9f199e
VenumadhavMamidala <venukanni38448@gmail.com>
2021-08-02 12:13:32 +0530

    commit

---
+++ src/components/Routes.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Employee from './training/Employee';
+import MyForm from './training/MyForm';
+import Home from './training/Home';
+import PageNotFound from './training/PageNotFound';
+import MaterialUiSample from './training/MaterialUiSample';
+import Header from './Header';
+import Footer from './Footer';
+import Welcome from './training/Welcome';
+import Login from './training/Login';
+
+const Routes = () => {
+    return (
+        <div>
+            <Router>
+                <div>
+                    <Header></Header>
+                    {/* <Link to="/home">Home</Link> {'  '}
+                <Link to="/emp">Employee</Link> {'  '}
+                <Link to="/form">MyForm</Link> {'  '}
+                <Link to="/mat">MaterialUiSample</Link> {'  '} */}
+                    <div className="container">
+                        <Switch>
+                            {/* add an exact path to base url  */}
+                            <Route exact path="/"> <Home /> </Route>
+                            <Route path="/welcome"> <Welcome /> </Route>
+                            <Route path="/emp"> <Employee /> </Route>
+                            <Route path="/form"><MyForm /></Route>
+                            <Route path="/home"><Home /></Route>
+                            <Route path="/mat"><MaterialUiSample /></Route>
+                            <Route path="/login"><Login/></Route>
+                            <Route path="/*"><PageNotFound /></Route>
+                        </Switch>
+                    </div>
+                </div>
+            </Router>
+            <Footer />
+        </div>
+    );
+};
+export default Routes;
---
+++ src/components/training/Welcome.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+// Function component 
+
+let fun = () => { return 'fun...' }
+
+
+let Welcome = (props) => {
+
+    return (
+        <div>
+            <h1 className="display-1 text-primary">Welcome Component</h1>
+            <p> {fun()} </p>
+        </div>
+    );
+}
+export default Welcome;
+
+
+// Class component 
+// class Welcome extends React.Component {
+    // construtor(props) {
+
+    // }
+
+//     render() {
+//     return <h1>Hello, {}</h1>;
+//   }
+// }
+// export default Welcome;
